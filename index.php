0743f12ad441ba628ed358cb6f396c46febfca25
Sherkios <Sherkiosp@gmail.com>
2021-11-21 18:29:42 +0500

    2 pflfybt

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 	<a href="f1.html">первое задание</a> <br>
-	<a href="laba2-2.php">второе задание</a> <br>
+	<a href="f2.html">второе задание</a> <br>
 	<a href="laba2-3.php">Третье задание</a> <br>
 	<a href="laba2-4.php">1 самостоятельная</a> <br>
 	<a href="laba2-5.php">Четвертое задание</a> <br>
