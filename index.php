5437c7eedba08465ba67326aaca13788a1059c74
Sherkios <Sherkiosp@gmail.com>
2021-11-21 18:17:47 +0500

    Первое задание

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/reset.css">
+	<title>Грищенко Игорь</title>
+</head>
+<body>
+	<a href="f1.html">первое задание</a> <br>
+	<a href="laba2-2.php">второе задание</a> <br>
+	<a href="laba2-3.php">Третье задание</a> <br>
+	<a href="laba2-4.php">1 самостоятельная</a> <br>
+	<a href="laba2-5.php">Четвертое задание</a> <br>
+	<a href="laba2-6.php">2 самостоятельная</a> <br>
+</body>
+</html>
